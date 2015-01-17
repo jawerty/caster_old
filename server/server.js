33ef8aff7507563aca2ade40d069b829b948a0b3
Jared Wright <jawerty210@gmail.com>
2015-01-17 02:17:26 -0500

    8080

--- server/server.js
+++ server/server.js
@@ -13,4 +13,4 @@ socket.on('open', function(){
   socket.on('close', function(){});
 });
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:8080/');
